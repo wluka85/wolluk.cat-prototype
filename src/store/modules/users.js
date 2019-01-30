a2fbe7e0a304de53a8be36a23e72ec8afded9a3e
wluka85 <wluka85@gmail.com>
2019-01-30 11:29:37 +0100

    Refactor in code

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -12,12 +12,12 @@ const actions = {
 const mutations = {
   addUser (state, userObject) {
     state.userList.push({
-       id: '',
-       displayName: '',
-       email: userObject.email,
-       password: userObject.password,
-       roles: {}
-     })
+      id: '',
+      displayName: '',
+      email: userObject.email,
+      password: userObject.password,
+      roles: {}
+    })
   }
 }
 export default {
