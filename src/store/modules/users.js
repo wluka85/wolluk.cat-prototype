bf18543cb14e42185b1ba722914afb1f1e2a2508
wluka85 <wluka85@gmail.com>
2019-01-31 12:36:32 +0100

    Add example user list and implement getter of current user name

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -1,8 +1,33 @@
 const state = {
-  userList: []
+  userList: [
+    {
+      'id': '-LWMS-pfJ937K4iwRl7Q',
+      'displayName': 'Tomek Drazek',
+      'email': 'tomek@wolluk.com',
+      'password': 'open-text-pass',
+      'roles': {
+        'admin': true // Admins other roles are irrelevant
+      }
+    },
+    {
+      'id': '-LWMS-pfJ937K4i4001l7Q', // this is id similar to Firebase (auto gen.)
+      'displayName': 'Janusz Cebula',
+      'email': 'janusz@example.com',
+      'password': 'open-text-pass',
+      'roles': {
+        'editor': true,
+        'translator': false,
+        'designer': false
+      }
+    }
+  ]
 }
 const getters = {
-
+  getCurrentUserName (state, getters, rootState) {
+    console.log(rootState)
+    let currentUserEmail = rootState.auth.user.email
+    return state.userList.filter(user => user.email === currentUserEmail)[0].displayName
+  }
 }
 const actions = {
 
