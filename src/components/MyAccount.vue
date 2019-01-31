ad3e4728db31c91c95e07af839257356e344737a
wluka85 <wluka85@gmail.com>
2019-01-31 14:14:15 +0100

    Add ability to change name of user in MyAccount component

--- src/components/MyAccount.vue
+++ src/components/MyAccount.vue
@@ -4,13 +4,13 @@
       v-flex.text-xs-center(xs12='', mt-5='')
         h1 My Account
       v-flex.text-xs-center(xs12='', sm10='', offset-sm1='', md2='', offset-md3='' mt-5='')
-        v-avatar(tile=true, color="grey lighten-4")
-          img(src="../assets/avatar.png", width='120px', alt="avatar")
+        //- v-avatar(tile=true, color="grey lighten-4")
+        img(src="../assets/avatar.png", width='120px', alt="avatar")
       v-flex(xs12='', sm10='', offset-sm1='', md3='', mt-5='')
-        form( @submit.prevent='userSetChanges')
+        form( @submit.prevent='userChangeName')
             v-layout(column='')
               v-flex
-                v-text-field#displayName(name='displayName', label='Display name' required='', v-model='displayName')
+                v-text-field#displayName(name='displayName', ref='name', label='Display name' required='', v-model='displayName')
               v-flex.text-xs-center(mt-3='')
                 v-btn(to='/home') Cancel
                 v-btn(color='primary', type='submit') Save
@@ -20,11 +20,12 @@
 export default {
   data () {
     return {
+      displayName: this.$store.getters['users/getCurrentUserName']
     }
   },
-  computed: {
-    displayName () {
-      return this.$store.getters['users/getCurrentUserName']
+  methods: {
+    userChangeName () {
+      this.$store.dispatch('users/userChangeName', this.$refs.name.value)
     }
   }
 }
